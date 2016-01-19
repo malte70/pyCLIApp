f54ba58691039aaef2b872895b954c87a908cad2
Malte Bublitz <malte70+github@mcbx.de>
2016-01-19 22:34:44 +0100

    New minor release

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@
 from distutils.core import setup
 
 setup(
-		name         = "cliapp",
-		version      = "0.2",
+		name         = "CLIApp",
+		version      = "0.2.1",
 		description  = "Utility classes for command line applications",
 		author       = "Malte Bublitz",
 		author_email = "malte70@mcbx.de",
