4a8e954acc207d919668eb672b75ff79e455398e
Malte Bublitz <malte70+github@mcbx.de>
2016-01-19 21:33:11 +0100

    Removed output of used Python interpreter in test.sh

--- test.sh
+++ test.sh
@@ -4,12 +4,6 @@ CUSTOM_PYTHON="yes"
 [[ -z $PYTHON ]] && CUSTOM_PYTHON="no"
 PYTHON=${PYTHON-python}
 
-echo "Testing with $(which $PYTHON) ($($PYTHON --version 2>&1))"
-if [[ $CUSTOM_PYTHON == "no" ]]
-then
-	echo "  Note: Set PYTHON to the executable to use to overwrite the default"
-fi
-
 ## Test 1: Module App; Feature Global options
 
 echo -n "Running Test #1... "
